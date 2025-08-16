35c480dc7e413b0acea7be4f83505c4524847332
Isaac Salas <iscprepa@gmail.com>
2025-08-15 20:58:11 -0600

    Hotfixes!!!
    Point counter now works!!!

--- Tutorial_completo/MainLevel/main.gd
+++ Tutorial_completo/MainLevel/main.gd
@@ -1,8 +1,20 @@
 extends Node3D
+class_name LevelManager
 @onready var label: Label = $Camera3D/UI/Label
+@export var Obstaculos : Node3D
 
 #manejador de escena, matiene el conteo de score
 var score = 0
+
+
+func _ready() -> void:
+	if Obstaculos != null:
+		var obstaculos_array : Array = Obstaculos.get_children()
+		for obstaculos in obstaculos_array:
+			if obstaculos is Obstaculo:
+				print(obstaculos)
+				obstaculos.deSpawn.connect(add_point)
+
 
 #reinicia la escena
 func reset():
@@ -10,5 +22,6 @@ func reset():
 
 #agrega un punto
 func add_point(points):
+	print("Entering Points!!!")
 	score += points
 	label.text=str("Score = ",score)
--- Tutorial_completo/Trigger/trigger.gd
+++ Tutorial_completo/Trigger/trigger.gd
@@ -1,12 +1,14 @@
 extends Area3D
+class_name Obstaculo
 signal gameOver
 signal deSpawn(value)
-var value = 5
+@export var value : int = 5
 @export var speed : float = 10
 @export_enum("Vertical","Horizontal") var type = 0
 # 				= 0 		= 1
 
 func _ready() -> void:
+	
 	# desplazar vertical
 	if type == 0:
 		position.y = randf_range(-7.5, 7.5)
@@ -19,7 +21,7 @@ func _ready() -> void:
 	position.z = -15
 	speed += 0.3
 	
-func _on_body_entered(body: Player) -> void:
+func _on_body_entered(body) -> void:
 	# game over detection
 	if body is Player:
 		gameOver.emit()
